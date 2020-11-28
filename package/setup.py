08d0ace299fb4dfa7c7a27b1680362988c8bcb8d
Marton Soos <marton.soos@secrethub.io>
2020-11-28 15:57:20 +0100

    Add commands for packaging win64 client in a wheel

---
+++ package/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="secrethub",
+    version="0.0.1",
+    author="SecretHub",
+    description="Python client for the SecretHub Secrets Management API",
+    url="https://secrethub.io",
+    packages=['secrethub'],
+    python_requires='>=3.9',
+)
