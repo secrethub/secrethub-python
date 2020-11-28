80c8e68dc944fe7397d7fa9a96ed42603153c44b
Marton Soos <marton.soos@secrethub.io>
2020-11-28 16:10:06 +0100

    Add dll and pyd to package

--- package/setup.py
+++ package/setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
 setuptools.setup(
     name="secrethub",
     version="0.0.1",
@@ -7,5 +11,9 @@ setuptools.setup(
     description="Python client for the SecretHub Secrets Management API",
     url="https://secrethub.io",
     packages=['secrethub'],
+    package_data={
+        'secrethub': ['Client.dll', '_secrethub.pyd'],
+    },
+    distclass=BinaryDistribution,
     python_requires='>=3.9',
 )
