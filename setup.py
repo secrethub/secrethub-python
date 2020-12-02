71ca4c2162b46e47f2d0ae99af436d6459203d6c
Marton Soos <marton.soos@secrethub.io>
2020-12-02 11:18:52 +0100

    Build wheel for win32

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, Extension
+from sys import platform
+
+extension=''
+if platform == "linux" or platform == "linux2":
+    extension = '.so'
+elif platform == "darwin":
+    extension = '.dylib'
+elif platform == "win32":
+    extension = '.lib'
 
 setup(
     name="secrethub",
@@ -6,7 +15,7 @@ setup(
     author="SecretHub",
     description="Python client for the SecretHub Secrets Management API",
     url="https://secrethub.io",
-    ext_modules=[Extension('_secrethub', ['secrethub.i'], extra_objects=['Client.so'])],
+    ext_modules=[Extension('_secrethub', ['secrethub.i'], extra_objects=['Client'+extension])],
     py_modules=['secrethub'],
     python_requires='>=3.8',
 )
