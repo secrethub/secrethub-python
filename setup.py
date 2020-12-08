0edc95eea4f6f0afa13c90b729ada6bf919018e7
Marton Soos <marton.soos@secrethub.io>
2020-12-08 14:54:35 +0100

    Try to include Client.pyd as package data

--- setup.py
+++ setup.py
@@ -1,26 +1,41 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, Distribution
 from sys import platform
 
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
 extra_obj_extension=''
-package_data = {}
 if platform == "linux" or platform == "linux2":
     extra_obj_extension = '.so'
 elif platform == "darwin":
     extra_obj_extension = '.dylib'
 elif platform == "win32":
     extra_obj_extension = '.lib'
-    package_data = {
-        'secrethub': ['Client.pyd'],
-    }
 
-setup(
-    name="secrethub",
-    version="0.0.7",
-    author="SecretHub",
-    description="Python client for the SecretHub Secrets Management API",
-    url="https://secrethub.io",
-    ext_modules=[Extension('_secrethub', ['secrethub.i'], extra_objects=['Client'+extra_obj_extension])],
-    py_modules=['secrethub'],
-    package_data=package_data,
-    python_requires='>=3.8',
-)
+if platform != "win32":
+    setup(
+        name="secrethub",
+        version="0.0.7",
+        author="SecretHub",
+        description="Python client for the SecretHub Secrets Management API",
+        url="https://secrethub.io",
+        ext_modules=[Extension('_secrethub', ['secrethub.i'], extra_objects=['Client'+extra_obj_extension])],
+        py_modules=['secrethub'],
+        python_requires='>=3.8',
+    )
+else:
+    setup(
+        name="secrethub",
+        version="0.0.7",
+        author="SecretHub",
+        description="Python client for the SecretHub Secrets Management API",
+        url="https://secrethub.io",
+        ext_modules=[Extension('_secrethub', ['secrethub.i'], extra_objects=['Client'+extra_obj_extension])],
+        py_modules=['secrethub'],
+        package_data={
+            'secrethub': ['Client.pyd'],
+        },
+        distclass=BinaryDistribution
+        python_requires='>=3.8',
+    )
