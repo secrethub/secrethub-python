e539c109224b6f05658921931719e1bb59af2d25
Marton Soos <marton.soos@secrethub.io>
2020-12-09 15:28:21 +0100

    Remove python version requirement

--- setup.py
+++ setup.py
@@ -17,5 +17,4 @@ setup(
     url="https://secrethub.io",
     ext_modules=[Extension('secrethub._secrethub', ['secrethub/secrethub.i'], extra_objects=['secrethub/Client'+extra_obj_extension])],
     packages=['secrethub'],
-    python_requires='>=3.8',
 )
