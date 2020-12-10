bb8c9a873246cc8521c587b73d3b9ddb57f2cf1d
Marton Soos <marton.soos@secrethub.io>
2020-12-10 19:36:22 +0100

    Fix unit test

--- test/test.py
+++ test/test.py
@@ -5,3 +5,6 @@ class TestSecretHubClient(unittest.TestCase):
 
     def test_read_string(self):
         self.assertEqual(secrethub.Client().read_string('secrethub/xgo/python/test/test'), 'foo')
+
+if __name__ == '__main__':
+    unittest.main()
