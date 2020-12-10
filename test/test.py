3a99401041847ab2039e87af18ecaf7a2189b674
Marton Soos <marton.soos@secrethub.io>
2020-12-10 11:20:57 +0100

    Add tests for linux, windows (x64 and x86) and macos 10.15 clients

---
+++ test/test.py
@@ -0,0 +1,7 @@
+import unittest
+import secrethub
+
+class TestSecretHubClient(unittest.TestCase):
+
+    def test_read_string(self):
+        self.assertEqual(secrethub.Client().read_string('secrethub/xgo/python/test/test'), 'foo')
