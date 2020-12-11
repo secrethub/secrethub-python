bd9152942db6587a7b8f49b369708d7e96799b21
Marton Soos <marton.soos@secrethub.io>
2020-12-11 11:46:07 +0100

    Add test for write and exists

--- test/test.py
+++ test/test.py
@@ -1,10 +1,24 @@
 import unittest
 import secrethub
+import random
+import string
 
 class TestSecretHubClient(unittest.TestCase):
 
     def test_read_string(self):
         self.assertEqual(secrethub.Client().read_string('secrethub/xgo/python/test/test'), 'foo')
+
+    def test_exists(self):
+        self.assertEqual(secrethub.Client().exists('secrethub/xgo/python/test/test'), True)
+        self.assertEqual(secrethub.Client().exists('secrethub/xgo/python/test/not-existent'), False)
+
+    def test_write_read_remove(self):
+        client = secrethub.Client()
+        secret_name = "secrethub/xgo/python/test/" + ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+        secret_value = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+        client.write(secret_name, secret_value)
+        self.assertEqual(client.read_string(secret_name), secret_value)
+        client.remove(secret_name)
 
 if __name__ == '__main__':
     unittest.main()
